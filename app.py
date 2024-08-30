40eadcfd863d33409be4ba47f913a3dc6f681180
AndPage <4.2@web.de>
2024-08-31 01:03:51 +0200

    add SequenceCreator

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from src.FileSelectorApp import FileSelectorApp
 from src.XMLReader import XMLReader
+from src.SequenceCreator import SequenceCreator
 import os
 
 if __name__ == "__main__":
     fileSelectorApp = FileSelectorApp()
     selected_file = fileSelectorApp.get_selected_file_name()
-    print(f"Selected file: {os.path.abspath(selected_file)}")
+    # print(f"Selected file: {os.path.abspath(selected_file)}")
 
     reader = XMLReader(os.path.abspath(selected_file))
     reader.load_xml()
     parsed_xml = reader.getParsedXML()
-    print(parsed_xml)
+    # print(parsed_xml)
+
+    sequenceCreator = SequenceCreator(parsed_xml)
+    sequenceTableIds = sequenceCreator.getSequence()
+    print(sequenceTableIds)
+
+    print(*range(len(sequenceTableIds)))
---
+++ src/SequenceCreator.py
@@ -0,0 +1,41 @@
+class SequenceCreator:
+    relations: list = []
+    tableIds: list = []
+    tableIdSequence: dict = {}
+    level: int = 0
+
+    def __init__(self, dictAll: dict) -> None:
+        self.relations = dictAll["relation"]
+        self.tableIds = [d["id"] for d in dictAll["entity"]["table"]]
+        self.setSequence()
+
+    def setSequence(self):
+        self.setAllTablesWithoutRelations()
+        self.setAllTablesWithoutTarget()
+
+    def setAllTablesWithoutRelations(self):
+        rSource = list({d["source"] for d in self.relations})
+        rTarget = list({d["target"] for d in self.relations})
+        allTablesWithoutRelations = list(
+            set(self.tableIds) - set(rSource) - set(rTarget)
+        )
+
+        self.setNextLevel(allTablesWithoutRelations)
+
+    def setAllTablesWithoutTarget(self):
+        rTarget = list({d["target"] for d in self.relations})
+        allTablesWithoutTarget = list(set(self.tableIds) - set(rTarget))
+        self.setNextLevel(allTablesWithoutTarget)
+        if len(self.tableIds):
+            self.setAllTablesWithoutTarget()
+
+
+    def setNextLevel(self, tableIds: list = []):
+        if len(tableIds):
+            self.tableIdSequence[self.level] = tableIds
+            self.tableIds = list(set(self.tableIds) - set(tableIds))
+            self.relations = [d for d in self.relations if d['source'] not in tableIds]
+            self.level += 1
+
+    def getSequence(self) -> dict:
+        return self.tableIdSequence
